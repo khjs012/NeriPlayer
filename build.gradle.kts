352f4edb46ecf75c027f12ce6f2c54c38279a1a0
Kim <133832205+khjs012@users.noreply.github.com>
2026-03-24 01:05:35 +0800

    Centralize desktop plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    kotlin("jvm") version "2.2.0" apply false
+    id("org.jetbrains.compose") version "1.8.2" apply false
 }
